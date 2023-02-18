cd81aabba01b93f4c91210f3c74b3dcdc36f7e50
Hammad Faiz <hammadfaiz172@gmail.com>
2023-02-17 23:10:41 -0500

    update users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,27 +10,43 @@ class UsersController < ApplicationController
 
   # GET /users/1
   def show
-    render json: @user
+    @current_user = User.find(session[:user_id])
+    render json: @current_user
   end
 
   # POST /users
   def create
-    @user = User.new(user_params)
-
-    if @user.save
-      render json: @user, status: :created, location: @user
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+      render json: user, status: :created
     else
-      render json: @user.errors, status: :unprocessable_entity
+      render json: {error: 'Email Taken, Please Login'}, status: 422
     end
   end
+  # def create
+  #   @user = User.new(user_params)
+
+  #   if @user.save
+  #     render json: @user, status: :created, location: @user
+  #   else
+  #     render json: @user.errors, status: :unprocessable_entity
+  #   end
+  # end
 
   # PATCH/PUT /users/1
   def update
-    if @user.update(user_params)
-      render json: @user
-    else
-      render json: @user.errors, status: :unprocessable_entity
+    user = User.find_by(id: session[:user_id])
+    if user.update(patch_params)
+      render json: user, status: :created
+    else 
+      render json: {error: user.errors.full_messages}
     end
+    # if @user.update(user_params)
+    #   render json: @user
+    # else
+    #   render json: @user.errors, status: :unprocessable_entity
+    # end
   end
 
   # DELETE /users/1
