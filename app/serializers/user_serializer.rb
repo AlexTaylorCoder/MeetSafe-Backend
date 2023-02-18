efdc68f856ec5e9850fca597c17f9b741c7669a4
Joseph Hirotsu <jhirotsu@gmail.com>
2023-02-18 16:05:22 -0500

    serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,11 +1,4 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :username, :password_digest, :email, :address, :state, :zipcode, :lat, :lng
 
-  has_many :user_exchanges
-  has_many :exchanges, through: :user_exchanges 
-  has_many :user_reviews 
-  has_many :reviews, through: :user_reviews
-
-  has_one_attached :avatar 
-  has_secure_password
 end
