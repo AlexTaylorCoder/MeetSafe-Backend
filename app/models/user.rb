f2fd18bcc7f4f9ffc0761a6d7a9d7777e1445d52
alextaylor <alextaylor515@gmail.com>
2023-02-17 22:23:03 -0500

    Added byebug and created secure password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+    has_many :user_exchanges
+    has_many :exchanges, through: :user_exchanges 
+    has_many :user_reviews 
+    has_many :reviews, through: :user_reviews
+
+    has_one_attached :avatar 
+    has_secure_password
 end
