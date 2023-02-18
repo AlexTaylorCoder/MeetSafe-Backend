684ef6f3d2f7d96e917e9ad4bfb1d24e6fd45061
Hammad Faiz <hammadfaiz172@gmail.com>
2023-02-17 22:17:05 -0500

    association work

--- app/models/exchange.rb
+++ app/models/exchange.rb
@@ -1,13 +1,6 @@
 class Exchange < ApplicationRecord
-<<<<<<< HEAD
     has_many :user_exchanges 
     has_many :users, through: :user_exchanges
     has_many :reviews 
     has_many :user_reviews, through: :reviews
-=======
-  has_many :user_exchanges
-  has_many :users, through: :user_exchanges
-  has_many :reviews
-  has_many :user_reviews, through: :reviews
->>>>>>> 3e2da3ab0799b24ad52ce119beb9de3ad1cb7644
 end
