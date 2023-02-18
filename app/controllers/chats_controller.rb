1f4772b8bdaf5b443ad54de8b67a779ad4cf6991
alextaylor <alextaylor515@gmail.com>
2023-02-18 16:30:23 -0500

    Started creating controllers and chat crud

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,2 +1,27 @@
 class ChatsController < ApplicationController
+    before_action :find_chat, except: [:create]
+        
+    def create
+        message = Chat.create(permitted)
+        message.broadcast
+        render json: message
+    end
+
+    def delete 
+        @chat.destroy
+    end
+
+    # def update 
+    #     @chat.update(mess)
+    # end
+
+    private 
+
+    def permitted 
+        params.permit(:user_id,:message,:exchange_id)
+    end
+
+    def find_chat 
+        @chat = chat.find(params[:exchange_id])
+    end
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,8 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username, :password_digest, :email, :address, :state, :zipcode, :lat, :lng
+  attributes :id, :username, :password_digest, :email, :address, :state, :zipcode, :lat, :lng, :avg_rating
+
+  def avg_rating 
+    object.reviews.average(:rating)
+  end
 
 end
